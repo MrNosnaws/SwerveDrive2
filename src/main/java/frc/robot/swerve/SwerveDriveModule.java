6ca3377381fa07d77bc09cdd85ba8c9aebd8a528
MrNosnaws <146788524+MrNosnaws@users.noreply.github.com>
2023-12-11 20:19:35 -0600

    init :)

---
+++ src/main/java/frc/robot/swerve/SwerveDriveModule.java
@@ -0,0 +1,14 @@
+package frc.robot.swerve;
+
+import com.ctre.phoenix.sensors.CANCoder;
+
+
+public final class SwerveDriveModule {
+    protected final CANCoder cancoder = new CANCoder(0);
+
+    
+
+
+
+    
+}
