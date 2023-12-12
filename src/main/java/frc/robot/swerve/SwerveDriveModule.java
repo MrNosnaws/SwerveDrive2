96132c7a7109e3b4a4c95dc0f69f5fcc87734f09
Maxwell Swanson <nosnaws1019@gmail.com>
2023-12-12 16:04:38 -0600

    Fixed

--- src/main/java/frc/robot/swerve/SwerveDriveModule.java
+++ src/main/java/frc/robot/swerve/SwerveDriveModule.java
@@ -6,16 +6,8 @@ import com.ctre.phoenix.sensors.CANCoder;
 
 public final class SwerveDriveModule {
     protected final CANCoder cancoder = new CANCoder(0);
-
-    private final TalonFX driveMotor1 = new TalonFX(1);
-    private final TalonFX driveMotor2 = new TalonFX(2);    
-    private final TalonFX driveMotor3 = new TalonFX(3);
-    private final TalonFX driveMotor4 = new TalonFX(4);
-
-    private final TalonFX steerMotor1 = new TalonFX(5);
-    private final TalonFX steerMotor2 = new TalonFX(6);
-    private final TalonFX steerMotor3 = new TalonFX(7);
-    private final TalonFX steerMotor4 = new TalonFX(8);
+    private final TalonFX driveMotor = new TalonFX(1);
+    private final TalonFX steerMotor = new TalonFX(2);
 
 
 }
