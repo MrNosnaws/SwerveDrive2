9eea25d199b9ba88f226fba67751705591d0561f
Maxwell Swanson <nosnaws1019@gmail.com>
2023-12-15 22:41:23 -0600

    set up basics of the drive command

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -1,10 +1,53 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class DriveCommands extends CommandBase {
-    
+    protected final DrivetrainSubsystem drivetrain;
+
+    //setting up needed stuff for the drive commands
+    private final DoubleSupplier xSupplier; //forward velocity
+    private final DoubleSupplier ySupplier; //sideward velocity
+    private final DoubleSupplier rotationSupplier; //angular velocity
+
+    //power limiters for the above
+    private final SlewRateLimiter xLimiter;
+    private final SlewRateLimiter yLimiter;
+    private final SlewRateLimiter rotaionLimiter;
+
+  
+
+    public DriveCommands (DoubleSupplier xSupplier, DoubleSupplier ySupplier, DoubleSupplier rotationSupplier, DrivetrainSubsystem drivetrain) {
+
+        // setting things up so we use local versions and don't get errors
+
+        this.drivetrain = drivetrain;
+
+        this.xSupplier = xSupplier;
+
+        this.ySupplier = ySupplier;
+
+        this.rotationSupplier = rotationSupplier;
+
+        //set up limits
+
+        //NOTE: SET UP LIMITERS TO WHATEVER VALUE
+
+        rotaionLimiter = new SlewRateLimiter(0);
+
+        yLimiter = new SlewRateLimiter(0);
+
+        xLimiter = new SlewRateLimiter(0);
+
+        addRequirements(drivetrain);
+    } 
+
+
+
 
 
     public void TestCommand(DrivetrainSubsystem drivetrain) {
@@ -13,6 +56,7 @@ public class DriveCommands extends CommandBase {
         drivetrain.testRunSteerMotor();
         addRequirements(drivetrain);
     }
+
 
 
 
