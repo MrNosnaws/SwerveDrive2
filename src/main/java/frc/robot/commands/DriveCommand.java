553f754810c1601457db364d510a91cb6ed8c1d2
MasonLilley <mason.lilley@gmail.com>
2023-12-15 18:26:33 -0600

    whoops

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -3,7 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
-public class DriveCommands extends CommandBase {
+public class DriveCommand extends CommandBase {
 
     public void TestCommand(DrivetrainSubsystem drivetrain) {
         //call setstates on all swerve modules in given drivetrain
