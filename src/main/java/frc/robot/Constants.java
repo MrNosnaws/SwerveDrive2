8e2d820a30c508445c9f7656fa09dd60c7a53a69
MasonLilley <mason.lilley@gmail.com>
2023-12-16 22:42:06 -0600

    copy constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,11 +26,34 @@ public final class Constants {
     double frontLeftModuleSteerOffsetRadians = Math.toRadians(217.6);
   }
   public static class SwerveConstants {
-    double wheelDiamiter = 0.1016;
-    double driveReduction = (14.0 / 50.0) * (28.0 / 16.0) * (15.0 / 60.0);
-    boolean driveInverted = true;
-    double steerReduction = (15.0 / 32.0) * (10.0 / 60.0);
-    double falcon300RoundsPerMinute = 6380;
-    double ticksPerRotations = 2048;
+    public static double WHEEL_DIAMETER = 0.1016;
+    public static double DRIVE_REDUCTION = (14.0 / 50.0) * (28.0 / 16.0) * (15.0 / 60.0);
+    public static double STEER_REDUCTION = (15.0 / 32.0) * (10.0 / 60.0);
+    public static double FALCON_300_ROUNDS_PER_MINUTE = 6380;
+    public static double TALONFX_VELOCITY_CONSTANT = 217;
+
+    public static boolean driveInverted = true;
+    public static boolean steerInverted = true;
+
+    //stolen from firefly idkw hat they do
+    //public static double drivePosConversionFactor = (Math.PI * WHEEL_DIAMETER / TICKS_PER_ROTATION) * DRIVE_REDUCTION;
+    //public static double driveVelocityConversionFactor = drivePosConversionFactor / 60.0;
+    
+    public static class Channels {
+      public static int FRONT_LEFT_DRIVE_MOTOR_CHANNEL = 0;
+      public static int FRONT_RIGHT_DRIVE_MOTOR_CHANNEL = 0;
+      public static int BACK_LEFT_DRIVE_MOTOR_CHANNEL = 0;
+      public static int BACK_RIGHT_DRIVE_MOTOR_CHANNEL = 0;
+
+      public static int FRONT_LEFT_STEER_MOTOR_CHANNEL = 0;
+      public static int FRONT_RIGHT_STEER_MOTOR_CHANNEL = 0;
+      public static int BACK_LEFT_STEER_MOTOR_CHANNEL = 0;
+      public static int BACK_RIGHT_STEER_MOTOR_CHANNEL = 0;
+
+      public static int FRONT_LEFT_CANCODER_CHANNEL = 0;
+      public static int FRONT_RIGHT_CANCODER_CHANNEL = 0;
+      public static int BACK_LEFT_CANCODER_CHANNEL = 0;
+      public static int BACK_RIGHT_CANCODER_CHANNEL = 0;
+      }
   }
 }
